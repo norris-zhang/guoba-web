61e9f5d4b4b779afc867206c5a53c50354f6cefa
norris-zhang <norriszhang@outlook.com>
2021-07-02 21:56:07 +1200

    Import bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
 import { NavigatorComponent } from './navigator/navigator.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { NavigatorComponent } from './navigator/navigator.component';
     NavigatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
